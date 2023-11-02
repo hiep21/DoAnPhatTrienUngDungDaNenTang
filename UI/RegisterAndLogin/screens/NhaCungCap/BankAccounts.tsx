96cb24d9e395ebfa528c7da057071e4789be2604
hieutrungct <106019570+hieutrungct@users.noreply.github.com>
2023-11-02 10:36:01 +0700

    update UI

--- UI/RegisterAndLogin/screens/NhaCungCap/BankAccounts.tsx
+++ UI/RegisterAndLogin/screens/NhaCungCap/BankAccounts.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet, Text, TextInput, TouchableOpacity, Alert, Image, ActivityIndicator } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Alert, Image, ActivityIndicator, Modal, ScrollView } from 'react-native';
 import { useState, useEffect, useCallback } from 'react';
 
 import { loginApi, configAxiosWithAccessToken, saveTokenToDevice, getAccessToken } from "../../services/todo";
 import Background from '../Users/Background';
 import { LoginData, BankAccount } from '../../services/interfaces/User.interface';
-import { CreateBankAccount,GetAccountByUser } from '../../services/todo';
+import { CreateBankAccount,GetAccountByUser,UpdateBankAccount } from '../../services/todo';
 
 
 
 const BankAccounts = ({ navigation }) => {
+    const [check ,setCheck] = useState<boolean>(false)
     const [banks, setBanks] = useState<BankAccount>()
     const [bank, setBank] = useState<BankAccount>({
         account: "",
@@ -23,14 +24,48 @@ const BankAccounts = ({ navigation }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [userVal, setUserVal] = useState(null);
 
-    
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [selectedGender, setSelectedGender] = useState<string>("");
+    const toggleModal = () => {
+        setIsModalVisible(!isModalVisible);
+    };
 
+    const selectGender = (Name: string) => {
+        setSelectedGender(Name);
+        setNameBank({
+            ...bank,
+            nameBank: Name
+        });
+        toggleModal();
+    };
+    const bankImages = {
+        'Agribank': require("../../assets/ImageBank/Agribank.png"),
+        'ACB Bank': require("../../assets/ImageBank/ACB Bank.png"),
+        'BIDV': require("../../assets/ImageBank/BIDV.png"),
+        'Co-opbank': require("../../assets/ImageBank/Co-opbank.png"),
+        'Eximbank': require("../../assets/ImageBank/Eximbank.png"),
+        'LienVietPostBank': require("../../assets/ImageBank/LienVietPostBank.png"),
+        'MBBank': require("../../assets/ImageBank/MBBank.png"),
+        'NCB bank': require("../../assets/ImageBank/NCB bank.png"),
+        'PVComBank': require("../../assets/ImageBank/PVComBank.png"),
+        'SacomBank': require("../../assets/ImageBank/SacomBank.png"),
+        'SCB Bank': require("../../assets/ImageBank/SCB Bank.png"),
+        'SeABank': require("../../assets/ImageBank/SeABank.png"),
+        'SHBBank': require("../../assets/ImageBank/SHBBank.png"),
+        'TechcomBank': require("../../assets/ImageBank/TechcomBank.png"),
+        'TPBank': require("../../assets/ImageBank/TPBank.png"),
+        'VIB Bank': require("../../assets/ImageBank/VIB Bank.png"),
+        'VietcomBank': require("../../assets/ImageBank/VietcomBank.png"),
+        'VietinBank': require("../../assets/ImageBank/VietinBank.png"),
+        'VPBank': require("../../assets/ImageBank/VPBank.png"),
+    };
 
     const UpBank = async () => {
         // if (!users.user || !users.password) {
         //     alert("Vui lòng điền đầy đủ thông tin đăng nhập.");
         //     return;
         // }
+        
 
         if (!isLoading) {
             setIsLoading(true)
@@ -112,19 +147,85 @@ const BankAccounts = ({ navigation }) => {
                             </View>
                         ) : null}
                         <Text style={styles.label}>Name Bank</Text>
-                        <TextInput value={bank.nameBank} onChangeText={(value) => {
-                            setBank({
-                                ...bank,
-                                nameBank: value
-                            }),
+                        <TouchableOpacity onPress={toggleModal} style={styles.dropdownButton}>
+                            <Text style={styles.selectedGenderText}>
+                                {selectedGender ? selectedGender : 'Agribank'}
+                            </Text>
+                        </TouchableOpacity>
 
-                                setNameBank(value)
-                        }} style={styles.input}  />
-                        {userVal != null && !bank.nameBank ? (
-                            <View>
-                                <Text style={styles.textError}>Vui lòng điền đầy đủ thông tin account.</Text>
+                        <Modal animationType="slide" transparent={true} visible={isModalVisible} onRequestClose={toggleModal}>
+                            <View style={styles.modalContainer}>
+                                <View style={styles.modalContent}>
+                                    <ScrollView style={{
+                                        width: "85%",
+                                        height: "55%",
+                                        marginLeft: 20,
+
+                                        }}>
+                                        <TouchableOpacity onPress={() => selectGender('Agribank')} style={styles.modalItem}>
+                                            <Text>Agribank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('ACB Bank')} style={styles.modalItem}>
+                                            <Text>ACB Bank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('BIDV')} style={styles.modalItem}>
+                                            <Text>BIDV</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('Co-opbank')} style={styles.modalItem}>
+                                            <Text>Co-opBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('Eximbank')} style={styles.modalItem}>
+                                            <Text>Eximbank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('LienVietPostBank')} style={styles.modalItem}>
+                                            <Text>LienVietPostBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('MBBank')} style={styles.modalItem}>
+                                            <Text>MBBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('NCB Bank')} style={styles.modalItem}>
+                                            <Text>NCB Bank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('PVComBank')} style={styles.modalItem}>
+                                            <Text>PVComBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('SacomBank')} style={styles.modalItem}>
+                                            <Text>SacomBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('SCB Bank')} style={styles.modalItem}>
+                                            <Text>SCB Bank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('SeABank')} style={styles.modalItem}>
+                                            <Text>SeABank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('SHBBank')} style={styles.modalItem}>
+                                            <Text>SHBBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('TechcomBank')} style={styles.modalItem}>
+                                            <Text>TechcomBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('TPBank')} style={styles.modalItem}>
+                                            <Text>TPBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('VIB Bank')} style={styles.modalItem}>
+                                            <Text>VIB Bank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('VietcomBank')} style={styles.modalItem}>
+                                            <Text>VietcomBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('VietinBank')} style={styles.modalItem}>
+                                            <Text>VietinBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => selectGender('VPBank')} style={styles.modalItem}>
+                                            <Text>VPBank</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
+                                            <Text>Close</Text>
+                                        </TouchableOpacity>
+                                    </ScrollView>
+                                </View>
                             </View>
-                        ) : null}
+                        </Modal>
 
                     </View>
                     <View style={styles.buttons}>
@@ -139,7 +240,10 @@ const BankAccounts = ({ navigation }) => {
 
                     </View>
                     <View>
-                        <Image source={require("../../assets/Icon/hinh-anh-ngan-hang-thuong-mai-la-gi-2.jpg")} style={styles.image} />
+                        <Image
+                            source={selectedGender ? bankImages[selectedGender] : require("../../assets/ImageBank/Agribank.png")}
+                            style={styles.image}
+                            />
                     </View>
                 </View>
 
@@ -232,7 +336,47 @@ const styles = StyleSheet.create({
     },
     textError: {
         color: "red"
-    }
+    },
+    dropdownButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1.25,
+        borderColor: '#777',
+        padding: 6,
+        borderRadius: 100,
+        justifyContent: 'space-between',
+        backgroundColor: "#5555",
+
+
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        width: '80%',
+    },
+    modalItem: {
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+    },
+    closeButton: {
+        marginTop: 10,
+        backgroundColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        alignItems: 'center',
+    },
+    selectedGenderText: {
+        flex: 1,
+        marginLeft: 10
+    },
 
 })
 export default BankAccounts
